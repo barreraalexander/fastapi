351c468add8315d34177efb009493ca96cf7d533
barreraalexander <barreraalexander93@gmail.com>
2022-07-18 21:39:36 -0400

    update

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 from app.routers import post, user, auth, vote
 
