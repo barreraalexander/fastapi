e2666625adb7b8da33ae6390bfd79b9d2e876475
barreraalexander <barreraalexander93@gmail.com>
2022-07-18 21:57:06 -0400

    update to base

--- app/main.py
+++ app/main.py
@@ -2,11 +2,29 @@ from fastapi import FastAPI
 from app import models
 from app.database import engine
 
+from fastapi.middleware.cors import CORSMiddleware
+
 # models.Base.metadata.create_all(bind=engine)
 
 from app.routers import post, user, auth, vote
 
 app = FastAPI(debug=True, reload=True)
+
+origins = [
+    "*"
+    # "https://www.google.com",
+    # "https://www.google.com/"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
